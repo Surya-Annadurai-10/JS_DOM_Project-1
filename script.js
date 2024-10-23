197691167cf51a2c663bf17319d92112635f24a7
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-10-23 08:46:30 +0530

    Dom project was completed

---
+++ script.js
@@ -0,0 +1,26 @@
+let box = document.getElementById("box");
+let container = document.querySelector(".container");
+
+// container Styles
+container.style.height = "100vh"
+container.style.width = "100%";
+container.style.display = "grid";
+container.style.placeItems = "center"
+container.style.background =  "linear-gradient(to right ,#ff0000 , #000";
+
+// box Styles
+
+box.style.background = "linear-gradient(to right ,#ff0000 , #000";
+box.style.margin = "20px";
+box.style.padding = "10px";
+box.style.fontSize = "18px";
+box.style.fontWeight = "bold";
+box.style.fontFamily = "sans-serif";
+box.style.height = "200px";
+box.style.width = "300px";
+box.innerText = "Im a Beautiful Container!!";
+box.style.display = "grid";
+box.style.placeItems = "center";
+box.style.color = "white"
+box.style.borderColor = "white"
+box.style.borderStyle = "solid"
